50d0ade368c9f154aa48051ed28c9d2ed7ed2c5c
jwe4 <jeweaver@gmail.com>
2020-08-20 14:10:51 -0700

    through 180

---
+++ src/components/Dropdown.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+/*
+  only want to respond to body click if clicked outside dropdwon
+*/
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      // is called first because wired up by addEvent
+      // console.log('body click');
+      // console.log(event.target);
+      if (ref.current.contains(event.target)) {
+        // if clicked on Dropdown, ignore
+        // all of this is to make sure that only respond
+        // below to clicks outside of dropdown
+        return;
+      }
+      setOpen(false);
+    };
+    document.body.addEventListener('click', onBodyClick);
+    return () => {
+      document.body.removeEventListener('click', onBodyClick);
+    };
+  }, []);
+
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+    return (
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => {
+          // console.log('item clicked');
+          onSelectedChange(option);
+        }}
+      >
+        {option.label}
+      </div>
+    );
+  });
+  return (
+    <div ref={ref} className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div
+          onClick={() => {
+            // console.log('dropdown clicked');
+            setOpen(!open);
+          }}
+          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,19 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -12,26 +24,43 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
 
       setResults(data.query.search);
     };
+    search();
+  }, [debouncedTerm]);
 
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 500);
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term]);
+  // useEffect(() => {
+  //   const search = async () => {
+  //     const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+  //       params: {
+  //         action: 'query',
+  //         list: 'search',
+  //         origin: '*',
+  //         format: 'json',
+  //         srsearch: term,
+  //       },
+  //     });
+
+  //     setResults(data.query.search);
+  //   };
+
+  //   if (term && !results.length) {
+  //     search();
+  //   } else {
+  //     const timeoutId = setTimeout(() => {
+  //       if (term) {
+  //         search();
+  //       }
+  //     }, 500);
+  //     return () => {
+  //       clearTimeout(timeoutId);
+  //     };
+  //   }
+  // }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
